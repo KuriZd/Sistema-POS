2e1380d147909547268613585976bcf1eb2db013
KuriZd <KuriZd@protonmail.com>
2026-02-16 23:15:38 -0600

    Refactor preload API

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,8 +1,24 @@
-import { ElectronAPI } from '@electron-toolkit/preload'
+import { contextBridge, ipcRenderer } from 'electron'
+import { electronAPI } from '@electron-toolkit/preload'
+import type { CreateProductPayload } from '../main/ipc/products.ipc'
+
+const api = {
+  products: {
+    create: (payload: CreateProductPayload) => ipcRenderer.invoke('products:create', payload)
+  }
+}
 
 declare global {
   interface Window {
-    electron: ElectronAPI
-    api: unknown
+    electron: typeof electronAPI
+    api: typeof api
   }
+}
+
+if (process.contextIsolated) {
+  contextBridge.exposeInMainWorld('electron', electronAPI)
+  contextBridge.exposeInMainWorld('api', api)
+} else {
+  window.electron = electronAPI
+  window.api = api
 }
